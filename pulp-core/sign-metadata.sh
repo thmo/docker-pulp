605b5b48dd17f996f30b6ce50640bbc43d9a9f8a
Philippe M. Chiasson <gozer@ectoplasm.org>
2021-04-21 10:07:04 -0400

    Try and make sign-metadata backwards compatible

--- pulp-core/sign-metadata.sh
+++ pulp-core/sign-metadata.sh
@@ -2,6 +2,7 @@
 
 FILE_PATH=$1
 SIGNATURE_PATH="$FILE_PATH.asc"
+PUBLIC_KEY=/tmp/public.key
 
 export GNUPGHOME=/var/lib/pulp/.gnupg
 ADMIN_ID=$(gpg --list-keys --with-colons | grep fpr: | head -n1 | cut -d: -f10)
@@ -13,7 +14,7 @@ gpg --quiet --batch --pinentry-mode loopback --yes --detach-sign -u "$ADMIN_ID" 
 # Check the exit status
 STATUS=$?
 if [[ $STATUS -eq 0 ]]; then
-    printf '{"file": "%s", "signature": "%s"}\n' "$FILE_PATH" "$SIGNATURE_PATH"
+    printf '{"file": "%s", "signature": "%s", "key" : "%s"}\n' "$FILE_PATH" "$SIGNATURE_PATH" "$PUBLIC_KEY"
 else
     exit $STATUS
 fi
